77d460b33d4eee57e796dfe0e417b61c7b4f95b4
Minnie <140838900+AtistarnMIT@users.noreply.github.com>
2023-11-17 14:27:22 -0500

    Update demo_stress_survey_v0.py

--- demo_stress_survey_v0.py
+++ demo_stress_survey_v0.py
@@ -9,7 +9,7 @@ stress_questions = [
     'In the last month, how often have you felt confident about your ability to handle your personal problems?',
     'In the last month, how often have you felt that things were going your way?',
     'In the last month, how often have you found that you could not cope with all the things that you had to do?',
-    'In the last month, have you often have you been able to control irritations in your life?',
+    'In the last month, how often have you been able to control irritations in your life?',
     'In the last month, how often have you felt that you were on top of things?',
     'In the last month, how often have you been angered because of things that were outside of your control?',
     'In the last month, how often have you felt difficulties were piling up so high that you could not overcome them?'
@@ -19,7 +19,7 @@ stress_questions = [
 reverse_quest = [
     'In the last month, how often have you felt confident about your ability to handle your personal problems?',
     'In the last month, how often have you felt that things were going your way?',
-    'In the last month, have you often have you been able to control irritations in your life?',
+    'In the last month, how often have you been able to control irritations in your life?',
     'In the last month, how often have you felt that you were on top of things?'
 ]
 
